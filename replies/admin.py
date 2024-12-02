fdfa8f3cd416dfbebba20c950d0fdd2f52d03568
Godson Jerry <88700239+jerry11671@users.noreply.github.com>
2024-12-02 15:59:44 +0100

    Update admin.py

--- replies/admin.py
+++ replies/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Replies
 
 
-
 @admin.register(Replies)
 class RepliesAdmin(admin.ModelAdmin):
     list_display = ['id', 'created', 'updated', 'active']
